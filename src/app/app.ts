d9da3efbc30b35d005f34865e09e4a97e4764292
G9Mares <gustavo99.mares@gmail.com>
2025-07-17 14:52:28 -0600

    feat: Layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { DisplayWheather } from './components/display-wheather/display-wheather';
+import { FormWheater } from './components/form-wheater/form-wheater';
+import { HistoricalComponent } from './components/historical-component/historical-component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [DisplayWheather, FormWheater, HistoricalComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
